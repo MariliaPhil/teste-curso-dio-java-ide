dd5552e4f879c185a486017b910d0519cee7ad00
MariliaPhil <mariliabphilippini@gmail.com>
2022-07-23 08:49:42 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,10 +11,7 @@ public class PrimeiroPrograma {
 		
 		System.out.println(gato);
 		System.out.println(livro);
-		// TODO Auto-generated method stub
-		/*int a = 2;
-		int b = 3;
-		System.out.println("Primeiro teste DIO Eclipse: " + (a+b));*/
+		
 	}
 }	
 	class Livros{
